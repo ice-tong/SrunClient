650ca124bb74d28ffc8da77e1fa6d281cc7cc763
ice-tong <32220263+ice-tong@users.noreply.github.com>
2020-12-20 20:20:40 +0800

    Update heartbeat.py

--- heartbeat.py
+++ heartbeat.py
@@ -1,23 +1,35 @@
 from srun import SrunClient
 import time
+import getpass
+import logging
 
+logging.basicConfig(
+    format='%(asctime)s.%(msecs)03d [%(filename)s:%(lineno)d] %(message)s',
+    datefmt='## %Y-%m-%d %H:%M:%S'
+)
+logging.getLogger("heartbeat").setLevel(logging.INFO)
+logger = logging.getLogger("heartbeat")
 
-time_interval = 60*10
+time_interval = 10 # 10s
 
+#USERNAME = input('username: ')
+#PASSWD = getpass.getpass('passwd: ')
+USERNAME = ''
+PASSWD = ''
 
 def check_online():
-    srun_client = SrunClient()
+    srun_client = SrunClient(print_log=False)
     if srun_client.check_online(): return
-    srun_client.username = 'username'
-    srun_client.passwd = 'passwd'
+    logger.info('NOT ONLINE, TRY TO LOGIN!')
+    srun_client.username = USERNAME
+    srun_client.passwd = PASSWD
     srun_client.login()
 
 
-def main(): pass
+def main(): ...
 
 
 if __name__ == "__main__":
     while 1:
         check_online()
         time.sleep(time_interval)
-        
